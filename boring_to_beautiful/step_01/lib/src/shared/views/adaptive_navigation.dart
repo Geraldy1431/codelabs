527905be3ab85aad4f1eb8bf4ba1a097bab7b670
geraldy taveras <geraldy0411@gmail.com>
2022-10-16 18:06:53 -0400

    cambios

--- boring_to_beautiful/step_01/lib/src/shared/views/adaptive_navigation.dart
+++ boring_to_beautiful/step_01/lib/src/shared/views/adaptive_navigation.dart
@@ -18,30 +18,36 @@ class AdaptiveNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, dimens) {
-        // Tablet Layout
-        // Add maxWidth constraint check
+        if (dimens.maxWidth >= 600) {
+          // Add maxWidth constraint check
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  extended: dimens.maxWidth >= 800,
+                  minExtendedWidth: 180,
+                  destinations: destinations
+                      .map((e) => NavigationRailDestination(
+                            icon: e.icon,
+                            label: Text(e.label),
+                          ))
+                      .toList(),
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: onDestinationSelected,
+                ),
+                Expanded(child: child),
+              ],
+            ),
+          );
+        }
         return Scaffold(
-          body: Row(
-            children: [
-              NavigationRail(
-                extended: dimens.maxWidth >= 800,
-                minExtendedWidth: 180,
-                destinations: destinations
-                    .map((e) => NavigationRailDestination(
-                          icon: e.icon,
-                          label: Text(e.label),
-                        ))
-                    .toList(),
-                selectedIndex: selectedIndex,
-                onDestinationSelected: onDestinationSelected,
-              ),
-              Expanded(child: child),
-            ],
+          body: child,
+          bottomNavigationBar: NavigationBar(
+            destinations: destinations,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
           ),
         );
-        // Add closing curly bracket
-
-        // Add return for mobile layout
       },
     );
   }
